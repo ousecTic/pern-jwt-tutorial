f17ca07c3605b174059a2cc4172d778fae070308
Henry Ly <36502535+l0609890@users.noreply.github.com>
2020-03-29 14:25:12 -0500

    Update dashboard.js

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -6,9 +6,16 @@ router.post("/", authorize, async (req, res) => {
   try {
     const user = await pool.query(
       "SELECT user_name FROM users WHERE user_id = $1",
-      [req.user.id]
-    );
-
+      [req.user.id] 
+    ); 
+    
+  //if would be req.user if you change your payload to this:
+    
+  //   function jwtGenerator(user_id) {
+  //   const payload = {
+  //     user: user_id
+  //   };
+    
     res.json(user.rows[0]);
   } catch (err) {
     console.error(err.message);
