abf4c046e58eb88017b6f3b4ce0cb19c475663c6
Henry Ly <henryly213@gmail.com>
2020-03-29 13:10:00 -0500

    clean comment in database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,19 @@
-CREATE DATABASE jwtauth;
+CREATE DATABASE authtodo;
 
 CREATE TABLE users(
-  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id uuid DEFAULT uuid_generate_v4(),
   user_name VARCHAR(255) NOT NULL,
-  user_email VARCHAR(255) NOT NULL,
-  user_password VARCHAR(255) NOT NULL
+  user_email VARCHAR(255) NOT NULL UNIQUE,
+  user_password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(user_id)
+);
+
+CREATE TABLE todo(
+  todo_id SERIAL,
+  user_id UUID ,
+  description VARCHAR(255),
+  PRIMARY KEY (todo_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 
