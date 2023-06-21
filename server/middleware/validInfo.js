3edf0acbe1af260dc8ed859c4b08d132be15d9e3
daniy310 <68704220+daniy310@users.noreply.github.com>
2023-06-21 12:11:01 +0300

    Updated validInfo.js
    Updated the main function in an arrow function, removed a console log and added res.status for each error.

--- server/middleware/validInfo.js
+++ server/middleware/validInfo.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res, next) {
+module.exports = (req, res, next) => {
   const { email, name, password } = req.body;
 
   function validEmail(userEmail) {
@@ -6,17 +6,16 @@ module.exports = function(req, res, next) {
   }
 
   if (req.path === "/register") {
-    console.log(!email.length);
     if (![email, name, password].every(Boolean)) {
-      return res.json("Missing Credentials");
+      return res.status(401).json("Missing Credentials");
     } else if (!validEmail(email)) {
-      return res.json("Invalid Email");
+      return res.status(401).json("Invalid Email");
     }
   } else if (req.path === "/login") {
     if (![email, password].every(Boolean)) {
-      return res.json("Missing Credentials");
+      return res.status(401).json("Missing Credentials");
     } else if (!validEmail(email)) {
-      return res.json("Invalid Email");
+      return res.status(401).json("Invalid Email");
     }
   }
 
